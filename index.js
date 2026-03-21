9655184077476ae738af4ca6568c9758e621baa6
Pat <patpat10504@gmail.com>
2026-03-21 17:43:03 +0700

    First Midleware

--- index.js
+++ index.js
@@ -1,14 +1,48 @@
 const express = require('express');
 const app = express();
+const { format } = require('date-fns');
 const path = require('path');
 const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
+app.get(['/', '/index', '/index.html'], (req, res) => {
     //res.sendFile('./views/index.html', { root: __dirname });
     console.log(req.host, req.url, req.method);
     res.sendFile(path.join(__dirname, 'views', 'index.html'));
 })
 
+app.get(['/new-page', '/new-page.html'], (req, res) => {
+    console.log(req.host, req.url, req.method);
+    res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+})
+
+app.get(['/old-page', '/old-page.html'], (req, res) => {
+    console.log(req.host, req.url, req.method);
+    res.redirect(301, '/new-page.html');
+})
+
+// route handler
+const one = (req, res, next) => {
+    console.log('one');
+    next();
+}
+const two = (req, res, next) => {
+    console.log('two');
+    next();
+}
+const three = (req, res, next) => {
+    console.log('three');
+    next();
+}
+app.get(['/test', '/test.html'], [one, two, three] , (req, res) => {
+    console.log(req.host, req.url, req.method);
+    res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('*spalt', (req, res) =>{
+    console.log(req.host, req.url, req.method);
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
 app.listen(PORT, () => {
-    console.log(`Server Running On Port: ${PORT}`);
+    console.log(`Server Running On Port: ${PORT} : ${format(new Date(), 'dd-MM-yyyy - HH:mm:ss')}`);
 });
